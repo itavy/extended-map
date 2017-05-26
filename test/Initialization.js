938b330506df1fa122f599a7ff7d050719aab9b7
Octavian Ionescu <itavyg@gmail.com>
2017-05-27 02:13:19 +0300

    - changed from proxied export to extend export (4x faster) - improved documentation
    Signed-off-by: Octavian Ionescu <itavyg@gmail.com>

--- test/Initialization.js
+++ test/Initialization.js
@@ -1,17 +1,17 @@
 'use strict';
 
 const expect = require('@itavy/test-utilities').getExpect();
-const extendedMap = require('../lib/v6x/index').ExtendedMap;
+const ExtendedMap = require('../lib/v6x/index').ExtendedMap;
 
 describe('Initialization', () => {
   it('Should return an instance of Map', (done) => {
-    const testMap = extendedMap();
+    const testMap = new ExtendedMap();
     expect(testMap).to.be.instanceOf(Map);
     done();
   });
 
   it('Set should work as before', (done) => {
-    const testMap = extendedMap();
+    const testMap = Reflect.construct(ExtendedMap, []);
     expect(testMap.set('testKey', 'testVal')).to.be.instanceOf(Map);
     expect(testMap.has('testKey')).to.be.equal(true);
     done();
@@ -19,7 +19,7 @@ describe('Initialization', () => {
 
   it('Get should work as before for existing value', (done) => {
     const testValue = Symbol('testSymbol');
-    const testMap = extendedMap();
+    const testMap = Reflect.construct(ExtendedMap, []);
     testMap.set('testKey', testValue);
     const testGetValue = testMap.get('testKey');
     expect(testGetValue).to.be.equal(testValue);
@@ -28,7 +28,7 @@ describe('Initialization', () => {
 
   it('Get should work as before for unexisting value', (done) => {
     const testValue = Symbol('testSymbol');
-    const testMap = extendedMap();
+    const testMap = Reflect.construct(ExtendedMap, []);
     testMap.set('testKey', testValue);
     const testGetValue = testMap.get('testKey2');
     expect(testGetValue).to.be.an('undefined');
